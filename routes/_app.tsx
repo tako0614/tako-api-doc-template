765ff9fd6b14673fa499591862807cc210bb7923
tako0614 <shoutatomiyama0614@gmail.com>
2024-07-27 18:36:37 +0900

    chore: Update routes to include lazy loading for images

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component }: PageProps) {
         <title>tako-api-doc-template</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
-      <body>
+      <body f-client-nav>
         <Component />
       </body>
     </html>
